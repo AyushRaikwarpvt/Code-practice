0dd5aa63d166ed3fd106d3e220a2a8f3c24deb0f
Ayush Raikwar <ayushraikwarpvt007@gmail.com>
2025-05-08 16:22:22 +0530

    Initial commit with IntelliJ project and folders

---
+++ src/Loops/Even_odd.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class Even_odd {
+    public static void main(String[] args) {
+
+        // Prompt the user to enter a number
+        int number = 20;
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+    }
+}
