72bfc5002d4d8729b36363d4e8f6898292e89b52
Ayush Raikwar <ayushraikwarpvt007@gmail.com>
2025-05-09 00:06:46 +0530

    Questions.java

--- src/Loops/Questions.java
+++ src/Loops/Questions.java
@@ -29,6 +29,48 @@ public class Questions {
     }
 
 
+//    Calculate the sum of numbers from 1 to 100 using a loop.
+    void SumFunc(int Start  , int End){
+       int count = 0;
+        for (int i = Start; i <= End; i++) {
+            count = count + i;
+        }
+        System.out.println("The of Sum Your Given Values " + count);
+    }
+
+//    Print the multiplication table of Any number using a for loop.
+
+    void Tablefun(int Tabele){
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println( Tabele  +" * "+ i + "= " + Tabele*i);
+        }
+    }
+    //Print all numbers from 10 to 1 in reverse using a loop.
+    void ReverseFun(int End){
+        for (int i = End; i >= 0; i--) {
+            System.out.println("Printing Reverse Gave you Number " +  i );
+        }
+    }
+
+//    Print only odd numbers from 1 to 20.
+    void Printodd(int Range){
+        for (int i = 0; i <= Range; i++){
+            if (i%2!=0){
+                System.out.println("Odd Number in our Range " + i);
+            }
+        }
+    }
+//    Find the factorial of a given number (e.g., 5 → 120).
+    void FatorialFun(int Fun){
+       int count = Fun;
+       int res = Fun;
+       int temp = 0 ;
+        for (int i = Fun; i >= 1; i--) {
+            temp = res * i-1;
+        }
+        System.out.println("That's Fatorial of your Given Number " + temp);
+    }
 
 
     public static void main(String[] args) {
@@ -39,7 +81,16 @@ public class Questions {
        obj.Printing(20);
         System.out.println("--------------");
         obj.PrintingDow(40);
-
+        System.out.println("--------------");
+        obj.SumFunc(1,10);
+        System.out.println("--------------");
+        obj.Tablefun(10);
+        System.out.println("--------------");
+        obj.ReverseFun(10);
+        System.out.println("--------------");
+        obj.Printodd(10);
+        System.out.println("--------------");
+        obj.FatorialFun(5);
     }
 
 }
